50b5c671cc816046e53db1a0924c9081f31b3fe0
Ubuntu <ubuntu@ip-172-31-35-14.us-east-2.compute.internal>
2017-12-12 06:41:31 +0000

    intial commit of file

---
+++ src/main/java/com/outlook/app/App.java
@@ -0,0 +1,13 @@
+package com.outlook.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
